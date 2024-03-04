b4e7d93dcffb1021f39be916023cd05c9a393637
Shridharann41 <shridharan41@gmail.com>
2024-03-04 21:50:59 +0000

    UI - grid and rectangles

--- src/sudoku/userinterface/UserInterfaceImpl.java
+++ src/sudoku/userinterface/UserInterfaceImpl.java
@@ -3,6 +3,8 @@ package sudoku.userinterface;
 import javafx.event.EventHandler;
 import javafx.scene.Group;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.paint.Color;
+
 import javafx.stage.Stage;
 import sudoku.problemdomain.Coordinates;
 import sudoku.problemdomain.SudokuGame;
@@ -29,22 +31,72 @@ public class UserInterfaceImpl implements IUserInterfaceContract.View,
     //The Key (<Key, Value> -> <Coordinates, Integer>) will be the HashCode of a given InputField for ease of lookup
 
     private HashMap <Coordinates, SudokuTextField> textFieldCoordinates;
+    private IUserInterfaceContract.EventListener listener;
+    private static final double WINDOW_Y = 732;
+    private static final double WINDOW_X = 668;
+    private static final double BOARD_PADDING = 50;
+    protected static final double BOARD_X_AND_Y = 576;
+
+    private static final Color WINDOW_BACKGROUND_COLOR = Color.rgb(0, 150, 136);
+    private static final Color BOARD_BACKGROUND_COLOR = Color.rgb(224, 242, 241);
+    private static final String SUDOKU = "Sudoku";
+
+
 
     public UserInterfaceImpl(Stage stage, Group root) {
         this.stage = stage;
-        this.root = root;
+        this.root = new Group();
+        this.textFieldCoordinates = new HashMap<>();
+        initializeUserInterface();
     }
 
-
-
-
-    @Override
-    public void handle(KeyEvent keyEvent) {
-
+    private void initializeUserInterface() {
+        drawBackground(root);
+        drawTitle(root);
+        drawSudokuBoard(root);
+        drawTextFields(root);
+        drawGridLines(root);
+        stage.show();
     }
+
+    private void drawGridLines(Group root) {
+        int xAndY = 114;
+        int index = 0;
+        while (index < 8) {
+            int thickness;
+
+            if (index == 2 || 5) {
+                thickness = 3;
+            } else {
+                thickness = 2;
+            }
+
+            Rectangle verticalLine = getLine(
+                    xAndY + 64 * index,
+                    BOARD_PADDING,
+                    BOARD_X_AND_Y,
+                    thickness
+
+            );
+        }
+    }
+
+    private void drawTextFields(Group root) {
+    }
+
+    private void drawSudokuBoard(Group root) {
+    }
+
+    private void drawTitle(Group root) {
+    }
+
+    private void drawBackground(Group root) {
+    }
+
 
     @Override
     public void setListener(IUserInterfaceContract.EventListener listener) {
+        this.listener = listener;
 
     }
 
@@ -65,6 +117,11 @@ public class UserInterfaceImpl implements IUserInterfaceContract.View,
 
     @Override
     public void showError(String Message) {
+
+    }
+
+    @Override
+    public void handle(KeyEvent keyEvent) {
 
     }
 }
